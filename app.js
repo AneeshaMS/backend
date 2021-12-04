b7a897a69768aaf15ddcbbd983f081d28ab0b3d9
AneeshaMS <aneeshamsani@gmail.com>
2021-12-04 21:25:39 +0530

    third commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app=new express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true,limit: '50mb'}))
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/libraryappfinal'));
 // app.use(express.urlencoded({limit: '50mb'}));
 // app.use(express.json());
 app.get('/api/books',function(req,res){
